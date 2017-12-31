55f6b679a07418ceffdb86ebeb4916de92cc1009
Anael Dahan <anael.assayag@gmail.com>
2017-12-31 09:49:12 +0200

    corrected bug about the remove button: Assigned him to the relevant class. Add new features to comment the post

--- main.js
+++ main.js
@@ -1,38 +1,95 @@
-debugger
 
 var posts = [];
-var generateNumber = 1
+var idNumber = 1;
+debugger
 
 
-function createPost(userInput, userId) {
-    posts.push({ text: userInput, number: userId })
+function pushPost(text, number) {
+    posts.push({ text: text, number: number })
 }
 
+function renderPosts() {
+    $('.posts').find('div').remove();
+    for (var i = 0; i < posts.length; i++) {
+        //add posts to the click event
+        var elmtPosts = $('<div>' + posts[i].text + '</div>')
+        $('.posts').append(elmtPosts)
+        elmtPosts.attr('data-id', posts[i].number)
+        // create button to remove comment
+        var rButton = $('<button type="button" class="remove">REMOVE</button>')
+        elmtPosts.append(rButton)
+
+
+        //add comment
+        var commentList = $('<ul></ul>')
+        elmtPosts.append(commentList)
+
+        var listComm = posts[i].comment
+        if (listComm) {
+            for (var j = 0; j < listComm.length; j++) {
+                commentList.append('<li>' + listComm[j].name + ' says: ' + listComm[j].text + '</li>')
+            }
+        }
+
+        // create button et form to comment
+        var cName = $('<input type="text" class="uName" placeholder="Your name"></input>')
+        elmtPosts.append(cName)
+        var cComment = $('<input type="text" class="uComment" placeholder="Add your comment"></input>')
+        elmtPosts.append(cComment)
+        var cbutton = $('<button type="button" class="uButton">Comment</button>')
+        elmtPosts.append(cbutton)
+
+
+
+
+    }
+    //add remove click
+    $('.remove').on('click', deletePost)
+
+    //add comment click
+    $('.uButton').on('click', addComment)
+
+
+}
+//function that publish post
+function postPost() {
+    var uText = $('#post-name').val()
+    var uNumber = idNumber++
+
+    pushPost(uText, uNumber);
+    renderPosts();
+}
+$('.add-post').on('click', postPost)
 
 function deletePost() {
-  var postId =  $(this).closest("div").data().id;
-  posts = posts.filter(function(e){return e.number != postId})
-  renderData();
+    var uId = $(this).closest('div').data().id;
+    posts = posts.filter(function (e) { return e.number != uId })
+    renderPosts()
 }
 
-function renderData() {
-    $('.posts').find('div').remove();
-    $('.posts').find('button').remove();
+function addComment() {
+    var commName = $(this).closest('div').find('.uName').val();
+    var commPost = $(this).closest('div').find('.uComment').val();
+    var postId = $(this).closest('div').data().id;
 
+    var commID = idNumber++
+
+
+    postComment(commName, commPost, postId)
+    renderPosts()
+
+}
+
+function postComment(commName, commPost, postId) {
+    var newComment = { name: commName, text: commPost }
     for (var i = 0; i < posts.length; i++) {
-        //Add post to the div
-        $('.posts').append('<div>' + posts[i].text + (' ') + '</div> ');
-        $('.posts').attr('data-id', posts[i].number)
-        // add remove button to the post
-        var input = $('<button type="button" class="remove">REMOVE</button>');
-        input.appendTo($('.posts'));
-        $('.remove').on('click', deletePost)
+        if (posts[i].number == postId) {
+            if (posts[i].comment) {
+                posts[i].comment.push(newComment)
+            }
+            else {
+                posts[i].comment = [newComment]
+            }
+        }
     }
 }
-
-$('.add-post').on('click', function () {
-    var uName = $('#post-name').val()
-    var uNumber = generateNumber++
-    createPost(uName, uNumber);
-    renderData();
-})
