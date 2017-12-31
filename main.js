6ab547114630d03a498624f6e6a20cf3b6d5fc98
Anael Dahan <anael.assayag@gmail.com>
2017-12-31 11:36:55 +0200

    test2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var posts = [];
 var idNumber = 1;
 debugger
 
-//test - new branch --
+//test - new branch --test 2
 function pushPost(text, number) {
     posts.push({ text: text, number: number })
 }
